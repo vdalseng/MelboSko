ee5b2790238455a49f3014f58d320b6f6493ee16
vdalseng <71254763+vdalseng@users.noreply.github.com>
2023-03-29 23:19:43 +0200

    Update burger-meny.js

--- code/burger-meny.js
+++ code/burger-meny.js
@@ -1,7 +1,15 @@
 function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-  }
-  
-  function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-  }
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
