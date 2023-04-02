904d53d192c6b8d5eace1c2b10c62b0ce4f5acc7
vdalseng <71254763+vdalseng@users.noreply.github.com>
2023-04-03 00:18:42 +0200

    Update burger-meny.js

--- code/burger-meny.js
+++ code/burger-meny.js
@@ -6,10 +6,22 @@ function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
 
-function openForm() {
-  document.getElementById("myForm").style.display = "block";
-}
+//Vi kan løse filtersystemet for mobil ved hjelp av bare èn knapp. Vi kan vise filteret når knappen trykkes
+// og endre teksten på knappen ved hjelp av en if/else.
 
-function closeForm() {
-  document.getElementById("myForm").style.display = "none";
+//Vi kan kombinere disse to løsningene under.
+
+//Denne gjør så vi kan endre tekst.
+
+function Filter( el ) {
+    if ( el.value === "Endre Filter" )
+        el.value = "Lukk";
+    else
+        el.value = "Endre Filter";
+    if (document.getElementById("myFilter").style.display === "block") {
+      document.getElementById("myFilter").style.display = "none";
+    } 
+    else {
+      document.getElementById("myFilter").style.display = "block";
+    }
 }
